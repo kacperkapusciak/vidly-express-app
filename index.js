c8def84ebc1521c95962e37b2cce5249df52905d
Kacper Kapuściak <kapusciak.k@gmail.com>
2019-05-17 14:12:32 +0200

    Connected Genres API to MongoDB

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
+const mongoose = require("mongoose");
 const express = require("express");
 const genres = require("./routes/genres");
+
+mongoose
+  .connect("mongodb://localhost/vidly", {
+    useNewUrlParser: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch(err => console.error("Could not connect to MongoDB..."));
 
 const app = express();
 app.use(express.json());
