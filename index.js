2d0fee1bbe440e48ab1893bc7c5f8443c2fae11a
Kacper Kapuściak <kapusciak.k@gmail.com>
2019-05-21 12:54:35 +0200

    Added rentals endpoint

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const genres = require("./routes/genres");
 const customers = require("./routes/customers");
 const movies = require("./routes/movies");
+const rentals = require("./routes/rentals");
 
 mongoose
   .connect("mongodb://localhost/vidly", {
@@ -17,5 +18,6 @@ app.use(express.json());
 app.use("/api/genres", genres);
 app.use("/api/customers", customers);
 app.use("/api/movies", movies);
+app.use("/api/rentals", rentals);
 
 app.listen(3001, () => console.log("Listening on port 3001..."));
