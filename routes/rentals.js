96e1f2cf3fd3b6a93b6f46e67183a69793ef5609
Kacper Kapuściak <kapusciak.k@gmail.com>
2019-05-21 13:25:23 +0200

    Two phase commit with Fawn package

--- routes/rentals.js
+++ routes/rentals.js
@@ -2,8 +2,11 @@ const { Rental, validate } = require("../models/rental");
 const { Movie } = require("../models/movie");
 const { Customer } = require("../models/customer");
 const mongoose = require("mongoose");
+const Fawn = require("fawn");
 const express = require("express");
 const router = express.Router();
+
+Fawn.init(mongoose);
 
 router.get("/", async (req, res) => {
   const rentals = await Rental.find().sort("-dateOut");
@@ -26,7 +29,8 @@ router.post("/", async (req, res) => {
     customer: {
       _id: customer._id,
       name: customer.name,
-      phone: customer.phone
+      phone: customer.phone,
+      isGold: customer.isGold
     },
     movie: {
       _id: movie._id,
@@ -34,12 +38,21 @@ router.post("/", async (req, res) => {
       dailyRentalRate: movie.dailyRentalRate
     }
   });
-  rental = await rental.save();
 
-  movie.numberInStock--;
-  movie.save();
+  try {
+    new Fawn.Task()
+      .save("rentals", rental)
+      .update("movies", { _id: movie._id }, {
+        $inc: { numberInStock: -1 }
+      })
+      .run();
 
-  res.send(rental);
+    res.send(rental);
+
+  } catch (ex) {
+    res.status(500).send("Something failed.");
+  }
+
 });
 
 router.get("/:id", async (req, res) => {
