1922b0d7a19b22df0b1b61473b99090356050d89
Kacper Kapuściak <39658211+kacperkapusciak@users.noreply.github.com>
2019-05-21 13:56:41 +0200

    Data relationships (#2)
    * Added movies endpoint
    
    * Added rentals endpoint
    
    * Bugfix
    
    * Two phase commit with Fawn package
    
    * Added objectId validation

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+const Joi = require("joi");
+Joi.objectId = require("joi-objectid")(Joi);
 const mongoose = require("mongoose");
 const express = require("express");
 const genres = require("./routes/genres");
 const customers = require("./routes/customers");
+const movies = require("./routes/movies");
+const rentals = require("./routes/rentals");
 
 mongoose
   .connect("mongodb://localhost/vidly", {
@@ -15,5 +19,7 @@ const app = express();
 app.use(express.json());
 app.use("/api/genres", genres);
 app.use("/api/customers", customers);
+app.use("/api/movies", movies);
+app.use("/api/rentals", rentals);
 
 app.listen(3001, () => console.log("Listening on port 3001..."));
