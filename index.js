b916b02c1a427f8b8413fca7d510935cc7cc0e62
Kacper Kapuściak <kapusciak.k@gmail.com>
2019-05-24 22:54:45 +0200

    Authenticating the user

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const config = require("config");
 const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
 const mongoose = require("mongoose");
@@ -7,6 +8,12 @@ const customers = require("./routes/customers");
 const movies = require("./routes/movies");
 const rentals = require("./routes/rentals");
 const users = require("./routes/users");
+const auth = require("./routes/auth");
+
+if ( !config.get("jwtPrivateKey") ) {
+  console.log("Fatal error: jwtPrivateKey is not defined.");
+  process.exit(1);
+}
 
 mongoose
   .connect("mongodb://localhost/vidly", {
@@ -23,5 +30,6 @@ app.use("/api/customers", customers);
 app.use("/api/movies", movies);
 app.use("/api/rentals", rentals);
 app.use("/api/users", users);
+app.use("/api/auth", auth);
 
 app.listen(3001, () => console.log("Listening on port 3001..."));
