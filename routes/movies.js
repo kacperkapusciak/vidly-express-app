1d1bde5641888f11edaa0a93c9b99f82215a7989
Kacper Kapuściak <kapusciak.k@gmail.com>
2019-05-25 17:21:32 +0200

    Protecting routes

--- routes/movies.js
+++ routes/movies.js
@@ -1,3 +1,4 @@
+const auth = require("../middleware/auth");
 const mongoose = require("mongoose");
 const express = require("express");
 const { Movie, validate } = require("../models/movie");
@@ -10,7 +11,7 @@ router.get("/", async (req, res) => {
   res.send(movies);
 });
 
-router.post("/", async (req, res) => {
+router.post("/", auth, async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
@@ -31,7 +32,7 @@ router.post("/", async (req, res) => {
   res.send(movie);
 });
 
-router.put("/:id", async (req, res) => {
+router.put("/:id", auth, async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
@@ -54,7 +55,7 @@ router.put("/:id", async (req, res) => {
   res.send(movie);
 });
 
-router.delete("/:id", async (req, res) => {
+router.delete("/:id", auth, async (req, res) => {
   const movie = await Movie.findByIdAndRemove(req.params.id);
 
   if (!movie) return res.status(404).send("Movie not found!");
