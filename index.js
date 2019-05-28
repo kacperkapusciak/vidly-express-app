c7a5043a884296848468ea55dcfb9e486ccd51a4
Kacper Kapuściak <kapusciak.k@gmail.com>
2019-05-28 22:02:11 +0200

    More refactoring

--- index.js
+++ index.js
@@ -1,43 +1,12 @@
-require("express-async-errors");
-const config = require("config");
 const winston = require("winston");
-require("winston-mongodb");
-const Joi = require("joi");
-Joi.objectId = require("joi-objectid")(Joi);
-const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 
+require("./startup/logging")();
 require("./startup/routes")(app);
+require("./startup/db")();
+require("./startup/config")();
+require("./startup/validation")();
 
-winston.handleExceptions(
-  new winston.transports.File({ filename: "uncaughtExceptions.log" })
-);
-
-process.on("unhandledRejection", ex => {
-  winston.error(ex.message, ex);
-  process.exit(1);
-});
-
-winston.add(winston.transports.File, { filename: "logfile.log" });
-winston.add(winston.transports.MongoDB, {
-  db: "mongodb://localhost/vidly",
-  level: "error"
-});
-
-if (!config.get("jwtPrivateKey")) {
-  console.log("Fatal error: jwtPrivateKey is not defined.");
-  process.exit(1);
-}
-
-mongoose
-  .connect("mongodb://localhost/vidly", {
-    useNewUrlParser: true,
-    useFindAndModify: false
-  })
-  .then(() => console.log("Connected to MongoDB..."))
-  .catch(err => console.error("Could not connect to MongoDB..."));
-
-
-
-app.listen(3001, () => console.log("Listening on port 3001..."));
+const port = process.env.PORT || 3001;
+app.listen(port, () => winston.info(`Listening on port ${port}...`));
